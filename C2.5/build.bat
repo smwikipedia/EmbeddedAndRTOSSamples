278958ed0149fb3a38c95c08ee1fe1effcde8137
Shao, Ming <smwikipedia@163.com>
2019-12-07 16:44:04 +0800

    - add a debug.bat for debug run.bat - add -g option in build.bat to generate debug symbols. - fix the missing build folder in build.bat.

--- C2.5/build.bat
+++ C2.5/build.bat
@@ -4,8 +4,8 @@ REM arm-none-eabi-as -mcpu=arm926ej-s  t1.S -o t5.o
 
 arm-none-eabi-as -mcpu=arm926ej-s  ts.S -o build/ts.o
 
-arm-none-eabi-gcc -mcpu=arm926ej-s -c t.c -o build/t.o
+arm-none-eabi-gcc -mcpu=arm926ej-s -g -c t.c -o build/t.o
 
-arm-none-eabi-ld ts.o t.o -T t.ld -o build/t.elf
+arm-none-eabi-ld build/ts.o build/t.o -T t.ld -o build/t.elf
 
 arm-none-eabi-objcopy -O binary build/t.elf build/t.bin
