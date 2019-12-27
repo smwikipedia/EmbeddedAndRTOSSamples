60d1317c7e1e93ba0f1dd9ce88f0f5a2e4035f31
Shao, Ming <smwikipedia@163.com>
2019-12-27 17:18:42 +0800

    -generate thumb instruction version just for comparison.

--- C2.5/build.bat
+++ C2.5/build.bat
@@ -1,10 +1,16 @@
+@echo off
 REM arm-none-eabi-gcc -S -mcpu=arm926ej-s t.c -o t1.S
 REM arm-none-eabi-as t1.S -o t1.o
 REM arm-none-eabi-as -mcpu=arm926ej-s  t1.S -o t5.o
+@echo on
 
-arm-none-eabi-as -mcpu=arm926ej-s  ts.S -o build/ts.o
+arm-none-eabi-as -mcpu=arm926ej-s ts.S -o build/ts.o
 
-arm-none-eabi-gcc -mcpu=arm926ej-s -g -c t.c -o build/t.o
+arm-none-eabi-gcc -mcpu=arm926ej-s -marm -g -c t.c -o build/t.o
+arm-none-eabi-gcc -mcpu=arm926ej-s -mthumb -g -c t.c -o build/t_thumb.o
+
+arm-none-eabi-gcc -mcpu=arm926ej-s -mthumb -S t.c -o t_thumb.S
+arm-none-eabi-gcc -mcpu=arm926ej-s -marm -S t.c -o t_arm.S
 
 arm-none-eabi-ld build/ts.o build/t.o -T t.ld -o build/t.elf
 
