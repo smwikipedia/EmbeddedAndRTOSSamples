1cdfd59c5ce381c939bc35a90a0f88593c56e433
smwikipedia <smwikipedia@163.com>
2025-05-08 17:54:59 +0800

    cm_os: initial commit of the cortex_m os code
    This commit contains start up assembly code, linker script, and makefile.
    
    Current code is for Cortex-M4. I checked several CMSIS-based Device Family
    Pack (DFP) from nRF and STM32. And created this  minimal codebase for startup.
    
    At first I attempted re-use the code from the nRF52840 DFP. And I did manage to
    build it. But it always trggier the HardFault on both the physical reelboard
    and QEMU netduioplus2.
    
    I don't know why.
    
    But with this miminal codebase, the HardFault goes away. So far so good.
    
    (cm = Cortex-M)
    
    Signed-off-by: smwikipedia <smwikipedia@163.com>

---
+++ cortex_m_os/kernel/main.c
@@ -0,0 +1,20 @@
+void SystemInit(void)
+{
+}
+
+void main(void)
+{
+    while(1);
+}
+
+void _start(void)
+{
+    /*
+    C/C++ initialiation,
+    or your own application specific main(),
+    or RTOS
+    ...
+    */
+
+    main();
+}
